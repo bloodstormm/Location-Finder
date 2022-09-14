022488c07108265d096cf8c431d9e0e20b92aa7b
Nicolas Malachias <nicolasmalaquias2015@hotmail.com>
2022-09-14 09:42:11 -0300

    LocationFinder 1.0

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import { Hero } from "./components/Hero";
+import { Form } from "./components/Form";
+import { Toaster } from "react-hot-toast";
+
+function App() {
+  return (
+    <div className="App">
+      <Toaster
+        position="bottom-center"
+        toastOptions={{
+          duration: 2000,
+          className:
+            "backdrop-blur-sm backdrop-brightness-110 bg-transparent text-white",
+        }}
+      />
+      <Hero />
+      <Form />
+    </div>
+  );
+}
+
+export default App;
